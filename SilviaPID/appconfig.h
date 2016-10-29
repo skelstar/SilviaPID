c7ef0d293b0bf82821060c191d3e6329df54d1cd
skelstar <skelstar@gmail.com>
2016-10-30 10:38:27 +1300

    Replaced with BasicOTA

---
+++ SilviaPID/appconfig.h
@@ -0,0 +1,7 @@
+const char* ssid = "LeilaNet2";
+const char* password = "ec1122%f*&";
+const char* host = "SilviaPID";
+
+    
+    // No authentication by default
+    // ArduinoOTA.setPassword((const char *)"123");
