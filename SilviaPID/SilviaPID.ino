388b28f36565ca8dc5a0ba6c1c5ed8d3450e61f4
skelstar <skelstar@gmail.com>
2017-02-12 13:36:54 +1300

    Installed. CoffeeSW channel not reading for some reason?

--- SilviaPID/SilviaPID.ino
+++ SilviaPID/SilviaPID.ino
@@ -2,19 +2,22 @@
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <EventManager.h>
+#include <TaskScheduler.h>
 #include <myPushButton.h>
 #include "appconfig.h"
 
 #define WATER_LOW   0
 #define COFFEE_SW   1
-#define FUEL_BAR   2
+#define FUEL_BAR    2
 
 /* ----------------------------------------------------------- */
 
 char versionText[] = "SilvaPID v1.2.0";
 
 #define PIXEL_PIN       15
+#define FLOODLIGHT_PIN  2
 #define NUMPIXELS       8          // pixel stick
+#define NUMFLOODPIXELS  4*4
 #define DEBUG_PIN       12
 #define BREW_PIN        13
 #define WATER_PIN       14
@@ -24,12 +27,14 @@ char versionText[] = "SilvaPID v1.2.0";
 /* ----------------------------------------------------------- */
 
 Adafruit_NeoPixel statusStick = Adafruit_NeoPixel(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel floodLight = Adafruit_NeoPixel(NUMFLOODPIXELS, FLOODLIGHT_PIN, NEO_GRB + NEO_KHZ800);
 
 #define STATUS_FLASHING     10
 
 uint32_t STATUS_COLOUR_OFF = statusStick.Color(0, 0, 0);
 uint32_t STATUS_ERROR_COLOUR = statusStick.Color(255, 0, 0);
 uint32_t STATUS_UNKNOWN_COLOUR = statusStick.Color(0, 0, 0);
+uint32_t FLOOD_RED_COLOR = floodLight.Color(255, 0, 0);
 
 /* ----------------------------------------------------------- */
 
@@ -97,7 +102,7 @@ void serviceEvent(int st);
 int getI2cChannelState();
 void setChannelPixel(Channel channel, int val);
 void setBlockChunkToColor(int index, int val);
-void listener_WaterLevel( int event, int waterlevel );
+void eventcallback_WaterLow( int event, int waterlevel );
 void listener_Flashing(int event, int statusBit);
 
 myPushButton debugPin(DEBUG_PIN, PULL_UP, 100000, HIGH, NULL);
@@ -113,6 +118,27 @@ MyWifiHelper wifiHelper(WIFI_HOSTNAME);
 
 
 /* ----------------------------------------------------------- */
+
+void tFlashWaterONCallback();
+void tFlashWaterOFFCallback();
+
+Scheduler runner;
+
+Task tFlashWater(800, TASK_FOREVER, &tFlashWaterONCallback, &runner, true);
+
+void tFlashWaterONCallback() {
+    statusStick.setPixelColor(channel[WATER_LOW].index, channel[WATER_LOW].color);
+    statusStick.show();
+    tFlashWater.setCallback(&tFlashWaterOFFCallback);
+}
+
+void tFlashWaterOFFCallback() {
+    statusStick.setPixelColor(channel[WATER_LOW].index, statusStick.Color(0,0,0));
+    statusStick.show();
+    tFlashWater.setCallback(&tFlashWaterONCallback);
+}
+
+/* ----------------------------------------------------------- */
 void setup() {
     
     Serial.begin(9600);
@@ -123,6 +149,12 @@ void setup() {
     statusStick.setPixelColor(0, statusStick.Color(0, 255, 0));
     statusStick.show();
 
+    floodLight.begin();
+    for (int i=0; i<NUMFLOODPIXELS; i++) {
+        floodLight.setPixelColor(i, FLOOD_RED_COLOR);
+    }
+    floodLight.show();
+
     Wire.begin();
 
     // debug mode?
@@ -131,17 +163,19 @@ void setup() {
     	Serial.println("DEBUG MODE");
     }
 
-    gEM.addListener(channel[WATER_LOW].eventCode, listener_WaterLevel);
-    gEM.addListener(channel[COFFEE_SW].eventCode, listener_CoffeeSw);
+    gEM.addListener(channel[WATER_LOW].eventCode, eventcallback_WaterLow);
+    gEM.addListener(channel[COFFEE_SW].eventCode, eventcallback_CoffeeSw);
     
     delay(1000);
 
+    runner.addTask(tFlashWater);
+    
     // get init states
     channel[WATER_LOW].state = getI2cChannelState(channel[WATER_LOW]);
-    listener_WaterLevel(channel[WATER_LOW].eventCode, channel[WATER_LOW].state);
+    eventcallback_WaterLow(channel[WATER_LOW].eventCode, channel[WATER_LOW].state);
 
     channel[COFFEE_SW].state = getI2cChannelState(channel[COFFEE_SW]);
-    listener_CoffeeSw(channel[COFFEE_SW].eventCode, channel[COFFEE_SW].state);
+    eventcallback_CoffeeSw(channel[COFFEE_SW].eventCode, channel[COFFEE_SW].state);
 
     wifiHelper.setupWifi();
     wifiHelper.setupOTA(WIFI_OTA_NAME);
@@ -163,6 +197,8 @@ void loop() {
     serviceBrewSwitch();
     //serviceFuelBarTick();
 
+    runner.execute();
+
     delay(100);
 }
 
@@ -175,7 +211,7 @@ void serviceWaterLevel() {
     if (stateChanged(waterCh, waterState)) {
         channel[WATER_LOW].prevState = channel[WATER_LOW].state;
         channel[WATER_LOW].state = waterState;
-        Serial.println("Water state changed");
+        // Serial.println("Water state changed");
         gEM.queueEvent(waterCh.eventCode, waterState);
     }
 }
@@ -188,7 +224,7 @@ void serviceBrewSwitch() {
     if (stateChanged(brewCh, brewState)) {
         channel[COFFEE_SW].prevState = channel[COFFEE_SW].state;
         channel[COFFEE_SW].state = brewState;
-        Serial.println("Brew state changed");
+        // Serial.println("Brew state changed");
         gEM.queueEvent(brewCh.eventCode, brewState);
 
         if (brewCh.state != brewCh.onState) {
@@ -229,16 +265,9 @@ int getI2cChannelState(Channel ch) {
 }
 
 void updateFuelBar(int seconds) {
-   
-    //statusStick.show();
 }
 
 void initFuelBar() {
-    // for (int i=0; i<statusStick.numPixels(); i++) {
-    //     statusStick.setPixelColor(i, statusStick.Color(0, 0, 0)); 
-    // }
-    // statusStick.show();
-    // channel[FUEL_BAR].state = 0;
 }
 
 void setChannelPixel(Channel ch) {
@@ -261,13 +290,19 @@ void setChannelPixel(Channel ch) {
 }
 
 /* ----------------------------------------------------------- */
-void listener_WaterLevel( int event, int state ) {
-    Serial.println("listener_WaterLevel");
+void eventcallback_WaterLow( int event, int state ) {
+    if (channel[WATER_LOW].state == channel[WATER_LOW].onState) {
+        Serial.println("Restarting tFlashWater");
+        tFlashWater.restart();
+    } else {
+        Serial.println("Disablng tFlashWater");
+        tFlashWater.disable();
+    }
     setChannelPixel(channel[WATER_LOW]);
 }
 
-void listener_CoffeeSw( int event, int state ) {
-    Serial.println("listener_CoffeeSw");
+void eventcallback_CoffeeSw( int event, int state ) {
+    Serial.println("eventcallback_CoffeeSw");
     setChannelPixel(channel[COFFEE_SW]);
 }
 
